7ab12bf8def027e55bbfbdbe1464d1189792cf92
Itzik Alkotzer <itzik.alkotzer@gmail.com>
2022-12-27 17:24:49 +0200

    list can be null

--- lib/list_live_data.dart
+++ lib/list_live_data.dart
@@ -1,93 +1,98 @@
 part of 'live_data.dart';
 
-class ListLiveData<T> extends MutableLiveData<List<T>>{
+class ListLiveData<T> extends MutableLiveData<List<T>?>{
 
-  ListLiveData({List<T>? initValue}): super(initValue: []);
+  ListLiveData({List<T>? initValue}): super(initValue: initValue);
 
-  T operator [](int index) {
-    return _value[index];
+  T? operator [](int index) {
+    return _initList?[index];
   }
-
+  
+  List<T> get _initList {
+    _value??=[];
+    return _value!;
+  }
+  
   void operator []=(int index, T value) {
-    _value[index] = value;
+    _initList[index] = value;
     notifyDataChanged();
   }
 
   void addItem(T value) {
-    _value.add(value);
+    _initList.add(value);
     notifyDataChanged();
   }
 
   void addItems(Iterable<T> iterable) {
-    _value.addAll(iterable);
+    _initList.addAll(iterable);
     notifyDataChanged();
   }
 
-  bool any(bool Function(T element) test) => _value.any(test);
+  bool any(bool Function(T element) test) => _initList.any(test);
 
-  Map<int, T> asMap() => _value.asMap();
+  Map<int, T> asMap() => _initList.asMap();
 
   void clear() {
-    _value.clear();
+    _initList.clear();
     notifyDataChanged();
   }
 
-  bool contains(Object? element) => _value.contains(element);
+  bool contains(Object? element) => _initList.contains(element);
 
-  T elementAt(int index) => _value.elementAt(index);
+  T elementAt(int index) => _initList.elementAt(index);
 
-  bool every(bool Function(T element) test) => _value.every(test);
+  bool every(bool Function(T element) test) => _initList.every(test);
 
-  T firstWhere(bool Function(T element) test, {T Function()? orElse}) => _value.firstWhere(test ,orElse: orElse);
+  T firstWhere(bool Function(T element) test, {T Function()? orElse}) => _initList.firstWhere(test ,orElse: orElse);
 
-  void forEach(void Function(T element) action) => _value.forEach(action);
+  void forEach(void Function(T element) action) => _initList.forEach(action);
 
-  int indexOf(T element, [int start = 0]) => _value.indexOf(element, start);
+  int indexOf(T element, [int start = 0]) => _initList.indexOf(element, start);
 
-  int indexWhere(bool Function(T element) test, [int start = 0]) => _value.indexWhere(test, start);
+  int indexWhere(bool Function(T element) test, [int start = 0]) => _initList.indexWhere(test, start);
 
   void insert(int index, T element) {
-    _value.insert(index, element);
+    _initList.insert(index, element);
     notifyDataChanged();
   }
 
   void insertAll(int index, Iterable<T> iterable) {
-    _value.insertAll(index, iterable);
+    _initList.insertAll(index, iterable);
     notifyDataChanged();
   }
 
-  bool get isEmpty => _value.isEmpty;
+  bool get isEmpty => _initList.isEmpty;
 
-  bool get isNotEmpty => _value.isNotEmpty;
+  bool get isNotEmpty => _initList.isNotEmpty;
 
-  Iterator<T> get iterator => _value.iterator;
+  Iterator<T> get iterator => _initList.iterator;
 
-  T lastWhere(bool Function(T element) test, {T Function()? orElse}) => _value.lastWhere(test, orElse: orElse);
+  T lastWhere(bool Function(T element) test, {T Function()? orElse}) => _initList.lastWhere(test, orElse: orElse);
 
   void removeItem(Object? value) {
-    _value.remove(value);
+    _initList.remove(value);
     notifyDataChanged();
   }
 
   void removeAt(int index) {
-    _value.removeAt(index);
+    _initList.removeAt(index);
     notifyDataChanged();
   }
 
   void removeWhere(bool Function(T element) test) {
-    _value.removeWhere(test);
+    _initList.removeWhere(test);
     notifyDataChanged();
   }
 
   @override
   void shuffle([Random? random]) {
-    _value.shuffle(random);
+    _initList.shuffle(random);
     notifyDataChanged();
   }
 
   @override
   void sort([int Function(T a, T b)? compare]) {
-    _value.sort(compare);
+    _initList.sort(compare);
     notifyDataChanged();
   }
 }
